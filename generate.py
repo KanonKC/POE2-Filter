e4fffe917daa951cd9e7eb1a3ceab7431c3dcd2d
KanonKC <kanonkce@gmail.com>
2025-01-15 15:47:20 +0700

    Script optimization

--- generate.py
+++ generate.py
@@ -1,26 +1,21 @@
 import os
 import shutil
 
-generalSounds = os.listdir('sounds/general')
+generalSounds = os.listdir('src/general')
 variantSounds = [soundDir for soundDir in os.listdir('sounds') if soundDir != 'general']
 
 filterFile = open('src/Vaal888.filter', 'r')
-filterData = filterFile.readlines()
+filterData = filterFile.read()
 
 for vsound in variantSounds:
     os.makedirs(f'dist/vaal888-{vsound}-soundpack', exist_ok=True)
     for sound in generalSounds:
-        shutil.copy(f'sounds/general/{sound}', f'dist/vaal888-{vsound}-soundpack')
+        shutil.copy(f'src/general/{sound}', f'dist/vaal888-{vsound}-soundpack')
     for sound in os.listdir(f'sounds/{vsound}'):
         shutil.copy(f'sounds/{vsound}/{sound}', f'dist/vaal888-{vsound}-soundpack')
 
-    newFilterDataLine = []
-    for data in filterData:
-        if "CustomAlertSound" in data:
-            insertIndex = data.find('"')+1
-            data = data[:insertIndex] + f"vaal888-{vsound}-soundpack/" + data[insertIndex:]
-        newFilterDataLine.append(data)
+    newFilterDataLine = filterData.replace('general/', f'vaal888-{vsound}-soundpack/')
         # newFilterData = 
 
     with open(f'dist/Vaal888-{vsound}.filter', 'w') as newFilterFile:
-        newFilterFile.write("".join(newFilterDataLine))
+        newFilterFile.write(newFilterDataLine)
